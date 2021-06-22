6201199d0ed748f76cb1abc3fed77b775d29fe98
Pranesh <praneshmeher@gmail.com>
2021-06-22 21:36:41 +0530

    commit

--- src/Playground/use-state.js
+++ src/Playground/use-state.js
@@ -3,22 +3,25 @@ import React, {useState, Fragment} from 'react'
 
 const initialstate = ()=>{
     console.log('initial state called')
-    return 0
+    return {count:0,limit:20}
 }
 
 const App = ()=>{
-    const[count, setCount] = useState(()=>initialstate())
+    const[state, setState] = useState(()=>initialstate())
+    const count = state.count
+    const limit = state.limit
 
     function increment(){
-        setCount((prevCount)=>{
-            return prevCount+1
+        setState((prevState)=>{
+            return {...prevState, count:prevState.count+1}
         })
     }
 
     console.log('functional component rendered')
     return(
         <Fragment>
-            Count : {count}
+            Count : {count} <br/>
+            Limit : {limit}
             <div>
                 <button onClick={()=>increment()}> 
                     Increment count
