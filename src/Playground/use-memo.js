c7f89c3a5b6d04624c54ba44abd98a13a20966fc
Pranesh <praneshmeher@gmail.com>
2021-06-23 01:01:37 +0530

    commit

--- src/Playground/use-memo.js
+++ src/Playground/use-memo.js
@@ -3,13 +3,12 @@ import React, { useState, Fragment, useMemo } from 'react'
 const App = ()=>{
     const[count, setCount] = useState(0)
 
-    function slow(){
+    const slowmemo = useMemo(()=>{
         console.log('slow called')
         for (let i = 0; i < 1000000000; i++) { }
         return 'memoized value'
-    }
-
-    const slowmemo = useMemo(()=>slow(),[])
+    },[])
+    
     console.log(slowmemo)
 
     function increment(){
