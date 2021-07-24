8e937f38f1484b22f14e029f665ba2d8afb78e07
Pranesh <praneshmeher@gmail.com>
2021-07-24 10:35:12 +0530

    commit

---
+++ src/Playground/clock.js
@@ -0,0 +1,18 @@
+import React, { Fragment, useState, useEffect } from 'react'
+
+const App = ()=>{
+    const [date, setDate] = useState(new Date().toLocaleTimeString())
+
+    useEffect(()=>{
+        setInterval(()=>{
+            setDate(new Date().toLocaleTimeString())
+        },[date])
+    },[])
+    return(
+        <Fragment>
+            {date}
+        </Fragment>
+    )
+}
+
+export default App
